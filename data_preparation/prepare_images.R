8a67ef9585248d0965331edd5607ea168a2c6ddb
ElenaAkhmerova <akhmerovaelena93@gmail.com>
2023-07-09 16:04:30 +0200

    use real data for age and sex tree graphs and add more text

--- data_preparation/prepare_images.R
+++ data_preparation/prepare_images.R
@@ -4,18 +4,53 @@ library(treemap)
 library(riskyr)
 library(igraph)
 
+### Read and explore data-------------------------------------------------------
+## Bread basket-----------------------------------------------------------------
+basket_dt <- fread("data/bread_basket.csv")
+
+## Titanic----------------------------------------------------------------------
+titanic_dt <- fread("titanic.csv")
+n_total <- nrow(titanic_dt)
+
+# Sex
+maenner <- nrow(titanic_dt[titanic_dt$Sex == "männlich",])#"male",])
+frauen <- nrow(titanic_dt[titanic_dt$Sex == "weiblich",])#"female",])
+
+maenner_ueberlebt <- nrow(titanic_dt[(titanic_dt$Sex == "männlich")#"male") 
+                                     & (titanic_dt$Survived == "überlebt"),])#1),])
+#maenner_gestorben <- nrow(titanic_dt[(titanic_dt$Sex == "male") 
+#                                     & (titanic_dt$Survived == 0),])
+#frauen_ueberlebt <- nrow(titanic_dt[(titanic_dt$Sex == "female") 
+#                                    & (titanic_dt$Survived == 1),])
+frauen_gestorben <- nrow(titanic_dt[(titanic_dt$Sex == "weiblich")#"female") 
+                                    & (titanic_dt$Survived == "nicht überlebt"),])#0),])
+
+anteil_maenner <- maenner / n_total # prev
+anteil_maenner_ueberlebt <- maenner_ueberlebt / maenner # sens
+anteil_frauen_gestorben <- frauen_gestorben / frauen # spec
+
+# Age
+children <- nrow(titanic_dt[titanic_dt$Age == "Kind",]) #< 20,])
+adults <- nrow(titanic_dt[titanic_dt$Age == "Erwachsene/r",]) #>= 20,])
+
+children_survived <- nrow(titanic_dt[(titanic_dt$Age == "Kind") #< 20)
+                                     & (titanic_dt$Survived == "überlebt"),])#1),])
+adults_deceased <- nrow(titanic_dt[(titanic_dt$Age == "Erwachsene/r") #>= 20)
+                                   & (titanic_dt$Survived == "nicht überlebt"),])#0),])
+
+anteil_kinder <- children / n_total # prev
+anteil_kinder_ueberlebt <- children_survived / children # sens
+anteil_adults_deceased <- adults_deceased / adults # spec
+
 ### Create frequency trees------------------------------------------------------
 ## Explanation------------------------------------------------------------------
 png("images/sex_tree.png", width = 800, height = 600, res = 120)              # Zahlen anpassen!!!
 my_txt <- init_txt(cond_true_lbl = "Männlich", cond_false_lbl = "Weiblich",
                    hi_lbl = "Überlebt", mi_lbl = "Nicht Überlebt", 
                    fa_lbl = "Überlebt", cr_lbl = "Nicht Überlebt")
-#my_col <- init_pal(N_col = "blue", hi_col = "gold", mi_col = "firebrick1", 
-#                   fa_col = "darkgreen", cr_col = "orange")
-sex_tree <- plot_prism(prev = 0.8, sens = 0.3, spec = 0.4, 
-           by = "cd", f_lbl = "nam", lbl_txt = my_txt,
- #          col_pal = my_col,
- f_lwd = .5, arr_c = 2)
+sex_tree <- plot_prism(prev = anteil_maenner, sens = anteil_maenner_ueberlebt, 
+                       spec = anteil_frauen_gestorben, by = "cd", f_lbl = "nam", 
+                       lbl_txt = my_txt, f_lwd = .5, arr_c = 2)
 print(sex_tree)
 dev.off()
 
@@ -34,20 +69,22 @@ dev.off()
 
 ## Creating training------------------------------------------------------------
 # Empty nodes-------------------------------------------------------------------
-png("images/age_treemap_gaps.png", width = 800, height = 600, res = 120)         # Zahlen anpassen!!!
-age_gaps_dt <- data.table(Kategorie = c(NA, "Erwachsen"),                        # for quiz mode both empty
-                          Unterkategorie = c("Überlebt", NA, NA, "Gestorben"),
-                          Häufigkeit = c(20, 15, 10, 5))                         # Zahlen anpassen!!!
-age_treemap_gaps <- treemap(age_gaps_dt, index = c("Kategorie", "Unterkategorie"), 
-                            vSize = "Häufigkeit", vColor = "Kategorie")
+png("images/age_tree_gaps.png", width = 800, height = 600, res = 120)         # Zahlen anpassen!!!
+my_txt <- init_txt(cond_true_lbl = "Kind", cond_false_lbl = "(1)______",
+                   hi_lbl = "Überlebt", mi_lbl = "Nicht Überlebt", 
+                   fa_lbl = "(2)______", cr_lbl = "(3)______")
+age_tree_gaps <- plot_prism(prev = anteil_kinder, sens = anteil_kinder_ueberlebt, 
+                            spec = anteil_adults_deceased, by = "cd", f_lbl = "nam", 
+                            lbl_txt = my_txt, f_lwd = .5, arr_c = 2)
 dev.off()
 
-png("images/age_treemap.png", width = 800, height = 600, res = 120)              # Zahlen anpassen!!!
-age_dt <- data.table(Kategorie = c("Kind", "Erwachsen"),
-                     Unterkategorie = c("Überlebt", "Gestorben", "Überlebt", "Gestorben"),
-                     Häufigkeit = c(20, 15, 10, 5))                              # Zahlen anpassen!!!
-age_treemap <- treemap(age_dt, index = c("Kategorie", "Unterkategorie"), 
-                       vSize = "Häufigkeit", vColor = "Kategorie")
+png("images/age_tree.png", width = 800, height = 600, res = 120)              # Zahlen anpassen!!!
+my_txt <- init_txt(cond_true_lbl = "Kind", cond_false_lbl = "Erwachsene/r",
+                   hi_lbl = "Überlebt", mi_lbl = "Nicht Überlebt", 
+                   fa_lbl = "Überlebt", cr_lbl = "Nicht Überlebt")
+age_tree <- plot_prism(prev = anteil_kinder, sens = anteil_kinder_ueberlebt, 
+                       spec = anteil_adults_deceased, by = "cd", f_lbl = "nam", 
+                       lbl_txt = my_txt, f_lwd = .5, arr_c = 2)
 dev.off()
 
 # Empty branches----------------------------------------------------------------
